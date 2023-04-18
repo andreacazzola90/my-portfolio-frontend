76e8f9ae8839359d248856b44563d8349630d12b
Andrea Cazzola <andrea.cazzola@getconnected.it>
2023-04-18 18:37:01 +0200

    test fetch data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home(data) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {JSON.stringify(data)}
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -121,4 +122,19 @@ export default function Home() {
       </div>
     </main>
   )
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://strapi-demo-gfes.onrender.com/api/products')
+  const posts = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      data: posts
+    },
+  }
 }
